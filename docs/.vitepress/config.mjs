fce37f7c755b1f86674dee76103ae30852a66637
c_shunyi <c_shunyi@outlook.com>
2025-10-21 23:09:43 +0800

    更新-修改tab

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,21 +2,21 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "shunyi-blog",
-  description: "A VitePress Site",
+  title: "博客小站",
+  description: "欢迎来到我的博客小站，在这里你可以了解到更加不一样的我哟「啾咪~」",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页', link: '/' },
+      { text: '关于我', link: '/about' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '文章',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: '文章1', link: '/article1' },
+          { text: '文章2', link: '/article2' }
         ]
       }
     ],
